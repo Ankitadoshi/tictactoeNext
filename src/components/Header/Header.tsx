06f6643cb7fe83bda78a1ec841f41541cc05f94b
Ankita Doshi <ankita.doshi@sap.com>
2023-07-03 17:47:17 +0530

    fix header file

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@
 "use client"
 import { useSelector } from 'react-redux'
 import styles from './Header.module.css'
+import React from 'react'
 
 export default function Header() {
   // const { rows, columns }  = useSelector((state: any) => state.details)
